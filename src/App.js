1271bc211fdb32d5a6e5195953cab8d19f2dbaec
chang yeon cho <ckddus1130@gmail.com>
2021-09-27 17:33:21 +0900

    state usage

--- src/App.js
+++ src/App.js
@@ -1,62 +1,36 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const foodILike = [
-{
-  id:1,
-  name:'bibibbap',
-  image: 'https://assets.dmagstatic.com/wp-content/uploads/2018/02/korean-food-guide-dallas.jpg',
-  rating:5,
-},
-{
-  id:2,
-  name:'kimchi',
-  image: 'https://mk0asiacryptotopf9lu.kinstacdn.com/wp-content/uploads/2020/09/kimchi-1068x534.jpg' ,
-  rating:3.8,
-},
-{
-  id:3,
-  name:'dduckbokhee',
-  image: 'https://media.cntraveler.com/photos/54ee97626450ad091fda0546/master/pass/Korean_Street_Food_Twigim_.jpg',
-  rating:4.9,
-},
-{
-  id:4,
-  name:'backban',
-  image: 'http://www.maangchi.com/wp-content/uploads/2015/02/koreanfood.jpg',
-  rating:4.7,
-},
-];
+// React의 state에 대해서 알아봅시다.
+// props와는 달리 동적 데이터를 가지고 작업을 하는 state
+class App extends React.Component {
 
-function Food({name, image, rating}) {
-  //console.log(props);
-  return (
-  <div>
-    <h1>I like {name}</h1>
-    <h4>평점: {rating} / 5.0</h4>
-    <img src={image} alt={name} />
-  </div>
-  )
-}
+  // 변화는 초기값을 아래에 작성해줍니다.
+    state = {
+      count: 0,
+  }
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,  
-  // rating은 number인데 string으로 작성되면 console에 경고가 뜸 
-  //이런식으로 props의 데이터가 타입에 맞는 데이터 인지 확인
+  add = () => {
+    // state를 바꾸고 싶을 땐 setState를 이용
+    // 아래와 같은 방법은 나쁜 방법
+    //this.setState({count: this.state.count + 1})
+    this.setState(current => ({count: current.count + 1}));
+  }
 
-}
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}));
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      {/* react의 각 elements들은 유일한 속성을 갖기를 원함 그래서 key값을 넣어줘야하는데 데이터api가 유일한 속성인 id를 이용해서 유일성을 지켜준다. 
-      넣어주면 Warning: Each child in a list should have a unique "key" prop. 해당에러가 사라집니다.* props로 사용하지 않고 react 내부에서 필요해서 사용함*/}
-        {foodILike.map((food) => <Food key={food.id} rating={food.rating} name={food.name} image={food.image} /> )}
-    </div>
-  )
+  }
+  render(){
+    return(
+      <div>
+        {/*  this.add는 클릭시 실행  this.add() 함수를 바로 호출하면 즉시 실행 */}
+        <h1>My count is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button> 
+      </div>
+    );
+    
+  };
 }
 
 
