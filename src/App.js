5cbb59322e5bbdfa94f4d8a36dd9e232f4696957
chang yeon cho <ckddus1130@gmail.com>
2021-09-27 12:26:32 +0900

    react map recipe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+const foodILike = [
+{
+  name:'bibibbap',
+  image: 'https://assets.dmagstatic.com/wp-content/uploads/2018/02/korean-food-guide-dallas.jpg',
+},
+{
+  name:'kimchi',
+  image: 'https://mk0asiacryptotopf9lu.kinstacdn.com/wp-content/uploads/2020/09/kimchi-1068x534.jpg' ,
+},
+{
+  name:'dduckbokhee',
+  image: 'https://media.cntraveler.com/photos/54ee97626450ad091fda0546/master/pass/Korean_Street_Food_Twigim_.jpg',
+},
+{
+  name:'backban',
+  image: 'http://www.maangchi.com/wp-content/uploads/2015/02/koreanfood.jpg',
+},
+];
+
+function Food({name, image}) {
+  //console.log(props);
+  return <div>
+    <h1>I like {name}</h1>
+    <img src={image} />
+  </div>
+  
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      {foodILike.map((food) => <Food name={food.name} image={food.image}/> )}
     </div>
-  );
+  )
+  
 }
 
 export default App;
